73d6f443aac44573ca5c37277d27d4a652d21593
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2026-01-02 22:59:20 +0700

    Menyusun EntrySiswaScreen dan integrasikan logika penyimpanan

--- app/src/main/java/com/example/firebase/view/HalamanEntry.kt
+++ app/src/main/java/com/example/firebase/view/HalamanEntry.kt
@@ -31,6 +31,42 @@ import com.example.firebase.viewmodel.EntryViewModel
 import com.example.firebase.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEntry.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            ) }
+    ) { innerPadding ->
+
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.addSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
 @Composable
 fun EntrySiswaBody(
