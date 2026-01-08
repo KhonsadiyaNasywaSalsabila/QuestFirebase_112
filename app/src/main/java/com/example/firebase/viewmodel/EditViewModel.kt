02ec9c2e2c7484f59a1b328c7758afef55a64f39
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2026-01-08 22:39:43 +0700

    Inisialisasi EditViewModel dan muat data siswa

---
+++ app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.firebase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebase.modeldata.DetailSiswa
+import com.example.firebase.modeldata.UIStateSiswa
+import com.example.firebase.modeldata.toDataSiswa
+import com.example.firebase.modeldata.toUiStateSiswa
+import com.example.firebase.view.route.DestinasiDetail
+import com.example.firebase.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+
+class EditViewModel(savedStateHandle: SavedStateHandle,private val repositorySiswa:
+    RepositorySiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+
+}
