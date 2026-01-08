db027ec4cdcded56f830cffcea07e0da84d5760a
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2026-01-08 22:36:28 +0700

    Implementasikan metode getSatuSiswa untuk mengambil detail siswa.

--- app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
@@ -26,5 +26,24 @@ class DetailViewModel(savedStateHandle: SavedStateHandle, private val repository
     var statusUIDetail:StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
 
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa(){
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositorySiswa.getSatuSiswa(idSiswa) )
+            }
+            catch (e: IOException){
+                StatusUIDetail.Error
+            }
+            catch (e: Exception){
+                StatusUIDetail.Error
+            }
+        }
+    }
+
 
 }
