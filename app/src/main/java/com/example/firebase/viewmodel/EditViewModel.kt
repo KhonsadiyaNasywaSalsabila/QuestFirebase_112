d22b5fb5ddfa08344010616889eab47e19e22bbd
Khonsadiya Nasywa Salsabila <nasywa9905@gmail.com>
2026-01-08 22:41:47 +0700

    Implementasikan editSatuSiswa untuk menyimpan perubahan

--- app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
@@ -38,5 +38,16 @@ class EditViewModel(savedStateHandle: SavedStateHandle,private val repositorySis
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            try {
+                repositorySiswa.editSatuSiswa(idSiswa,uiStateSiswa.detailSiswa.toDataSiswa
+                    ())
+                println("Update Sukses: $idSiswa")
+            } catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
+        }
+    }
 
 }
